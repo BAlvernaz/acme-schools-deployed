ab65832759d3951b7b7aa5505c664d3b5e0d8fff
BAlvernaz <balverna@gmail.com>
2019-07-30 00:12:17 -0700

    fixed an error

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -10,7 +10,7 @@ for(let i = 0; i < 10; i++) {
 
 const seed = async () => {
   await db.sync({force: true})
-  await Student.create({firstName: Admin, lastName: "Admin", email:"admin@admin.com", gpa: 4})
+  await Student.create({firstName: "Admin", lastName: "Admin", email:"admin@admin.com", gpa: 4, password: "password"})
   await studentArr.map(student => Student.create(student))
   await School.create({name: 'Cal Poly San Luis Obispo', image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/CalPoly_Seal.svg/1024px-CalPoly_Seal.svg.png'})
   await School.create({name: 'Cal Berkeley', image:'http://3b9m3d3keq0q4enwal2laffp.wpengine.netdna-cdn.com/wp-content/uploads/2014/12/UC-Berkeley.jpg'})
